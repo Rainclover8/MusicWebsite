675d779bacbcf9eb8c2742763b8d097163f7eaf1
merahab <ts.baran6161@gmail.com>
2024-02-04 22:27:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+window.sr = new ScrollReveal();
+
+// sr.reveal('.rowFoto');
+sr.reveal('.rowFoto');
+
+
+ScrollReveal().reveal('.featurette-heading',{ reset: true }, { delay : 700});
+ScrollReveal().reveal('.lead',{ reset: true }, { delay: 800});
+ScrollReveal().reveal('.baslik1',{ reset: true }, { delay: 900})
+ScrollReveal().reveal('.rowFoto',{ reset: true }, { delay: 1000 });
+
+const pageBtn = document.querySelectorAll('.cart')
+let btn = document.querySelectorAll('.btn')
